900ccd6bcbce9031ea13a1598556e67574d8b62b
André Alves Correia <andre2013aguabranca@gmail.com>
2024-05-13 10:52:29 -0300

    chore: sync with repo in Azure Devops

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 @echo off
 
-xcopy /E /Y ".\*" "C:\Program Files\nginx-1.24.0\html\turma-5\desafio-4\grupo-x\servico" && cd "C:\Program Files\nginx-1.24.0\html\turma-5\desafio-4\grupo-x\servico" && npm ci && npm run start:prod && cd "C:\Program Files\nginx-1.24.0" && nginx -s reload
+xcopy /E /Y ".\*" "C:\Program Files\nginx-1.24.0\html\dev\turma-5\desafio-4\grupo-1\aluguel" && cd "C:\Program Files\nginx-1.24.0\html\dev\turma-5\desafio-4\grupo-1\aluguel" && npm ci && npm run start:prod && cd "C:\Program Files\nginx-1.24.0" && nginx -s reload
 
 exit /b 0
